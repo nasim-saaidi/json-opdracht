7b287cbe22318e790e694c358dfc1fd71ea94fb2
nasim-saaidi <302263973@student.rocmondriaan.nl>
2023-02-18 13:38:00 +0100

    opdracht 3

--- js/main.js
+++ js/main.js
@@ -8,7 +8,29 @@ const pen = {
     barrel : 'https://www.montblanc.com/variants/images/34480784411808558/A/w2500.jpg',
 };
 
-// array = [pen.ink, pen.spring, pen.ballpoint, pen.inkChamber, pen.barrel]
-array = [pen]
+//opdracht 2
+array = [pen.ink, pen.spring, pen.ballpoint, pen.inkChamber, pen.barrel]
+
 
 console.log(array)
+
+//opdracht 3 
+function stringifyElements() {
+    for(let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
+}
+
+
+
+const objectOne = JSON.stringify(pen.ink);
+const objectTwo = JSON.stringify(pen.spring);
+const objectThree = JSON.stringify(pen.ballpoint);
+const objectFour = JSON.stringify(pen.inkChamber);
+const objectFive = JSON.stringify(pen.barrel);
+
+console.log(objectOne)
+console.log(objectTwo)
+console.log(objectThree)
+console.log(objectFour)
+console.log(objectFive)
