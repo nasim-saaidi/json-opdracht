2efbfb7811803012b598d36e389b18fa6be47d1f
nasim-saaidi <302263973@student.rocmondriaan.nl>
2023-02-18 13:10:31 +0100

    opdracht 1 & 2

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//opdracht 1
+
+const pen = {
+    ink : 1,
+    spring : 'an elastic object that stores mechanical energy',
+    ballpoint : 'pen',
+    inkChamber : 'tube with ink in it',
+    barrel : 'https://www.montblanc.com/variants/images/34480784411808558/A/w2500.jpg',
+};
+
+// array = [pen.ink, pen.spring, pen.ballpoint, pen.inkChamber, pen.barrel]
+array = [pen]
+
+console.log(array)
